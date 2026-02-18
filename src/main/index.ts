df19881b264dd1f5947f5480a80ff82168c4aac7
Nihal Singh <nihal.111@gmail.com>
2026-02-17 23:09:01 -0800

    fix: restore preload.js extension and db import path

--- src/main/index.ts
+++ src/main/index.ts
@@ -6,7 +6,7 @@ import {
   getAllLists, createList, updateList, deleteList, reorderList, moveList, getTaskCount,
   getInboxTasks, getInboxTaskCount, getTasksByList, createTask, updateTask, deleteTask, reorderTask, moveTask,
   calcSortKeyBetween,
-} from './db/index';
+} from './db';
 
 let mainWindow: BrowserWindow | null = null;
 
@@ -16,7 +16,7 @@ function createWindow(): void {
     height: 600,
     center: true,
     webPreferences: {
-      preload: path.join(__dirname, 'preload'),
+      preload: path.join(__dirname, 'preload.js'),
       contextIsolation: true,
       nodeIntegration: false,
     },
